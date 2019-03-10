d0695da80870d6b77aa75670ba23d9cd15af828c
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-03-10 10:01:12 +0700

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,13 @@ db = mongoose.connect(dbURI,{useNewUrlParser: true},function(err){
 
 })
 
+//  ********   Model define ***************
+var modelInputMaterialAndCost = require('./models/InputMaterialAndCost');
+var modelDepartment = require('./models/MasterDepartment');
+var modelMaterial = require('./models/MasterMaterial');
+var modelUnit = require('./models/MasterUnit');
+var modelUser = require('./models/MasterUser');
+
 var app = express();
 app.use(cors());
 
@@ -28,16 +35,16 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use(cookieParser());
-app.use(session({
-  name: 'JSESSION',
-  secret: 'kungpotato',
-  store: new MemoryStore(60 * 60 * 12),
-  resave: true,
-  saveUninitialized: true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(cookieParser());
+// app.use(session({
+//   name: 'JSESSION',
+//   secret: 'kungpotato',
+//   store: new MemoryStore(60 * 60 * 12),
+//   resave: true,
+//   saveUninitialized: true
+// }));
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -46,42 +53,56 @@ app.use(function(req, res, next) {
   next();
 });
 
-passport.use(new LocalStrategy(
-  function(username, password, done) {
-    modelUser.findOne({ username: username }, function (err, user) {
-      //console.log(user)
-      if (err) { return done(err); }
-      if (!user) { return done(null, false); }
-      if (!user.verifyPassword(password)) { return done(null, false); }
+// passport.use(new LocalStrategy(
+//   function(username, password, done) {
+//     modelUser.findOne({ username: username }, function (err, user) {
+//       //console.log(user)
+//       if (err) { return done(err); }
+//       if (!user) { return done(null, false); }
+//       if (!user.verifyPassword(password)) { return done(null, false); }
 
-      return done(null, user);
-    });
-  }
-));
-passport.serializeUser(function(user, done) {
-  //console.log('serializeUser')
-  done(null, user);
-});
-passport.deserializeUser(function(user, done) {
-  console.log('deserializeUser')
-  done(null, user);
-});
+//       return done(null, user);
+//     });
+//   }
+// ));
+// passport.serializeUser(function(user, done) {
+//   //console.log('serializeUser')
+//   done(null, user);
+// });
+// passport.deserializeUser(function(user, done) {
+//   console.log('deserializeUser')
+//   done(null, user);
+// });
 
 app.get('/', function(req, res){
     res.send('welcome to web API!');
 });
-app.post('/api/login',
-  passport.authenticate('local', { session: true }),
-  function(req, res) {
-    res.send(req.session.passport.user);
-  }
-);
-app.post('/api/logout', function (req, res) {
-  req.logout()
-  req.session.destroy(function (err) {
-    res.redirect("/")
-  })
-})
+// app.post('/api/login',
+//   passport.authenticate('local', { session: true }),
+//   function(req, res) {
+//     res.send(req.session.passport.user);
+//   }
+// );
+// app.post('/api/logout', function (req, res) {
+//   req.logout()
+//   req.session.destroy(function (err) {
+//     res.redirect("/")
+//   })
+// })
+
+//  ********   Routes define ***************
+InputMaterialAndCostRouter = require('./Routes/InputMaterialAndCostRoutes')(modelInputMaterialAndCost)
+DepartmentRouter = require('./Routes/deptRoutes')(modelDepartment)
+MaterialRouter = require('./Routes/materialRoutes')(modelMaterial)
+UnitRouter =  require('./Routes/unitRoutes')(modelUnit)
+UserRouter =  require('./Routes/userRoutes')(modelUser)
+
+app.use('/api/input', InputMaterialAndCostRouter);
+app.use('/api/department', DepartmentRouter);
+app.use('/api/material', MaterialRouter);
+app.use('/api/unit', UnitRouter);
+app.use('/api/register', UserRouter);
+// *******************************************
 
 
 app.listen(port, function(){
