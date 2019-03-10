0d23edfde5eac38a28aeface42600b843546f118
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-03-10 13:29:57 +0700

    Update app.js

--- app.js
+++ app.js
@@ -35,54 +35,49 @@ var port = process.env.PORT || 3000
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(session({
-  name: 'JSESSION',
-  secret: 'kungpotato',
-  store: new MemoryStore(60 * 60 * 12),
-  resave: true,
-  saveUninitialized: true
-}))
-app.use(passport.initialize())
-app.use(passport.session())
+// app.use(cookieParser())
+// app.use(session({
+//   name: 'JSESSION',
+//   secret: 'kungpotato',
+//   store: new MemoryStore(60 * 60 * 12),
+//   resave: true,
+//   saveUninitialized: true
+// }))
+// app.use(passport.initialize())
+// app.use(passport.session())
 
-passport.use(new LocalStrategy(
-  (username, password, done) => {
-    modelUser.findOne({ username: username }, (err, user) => {
-      // console.log(user)
-      if (err) { return done(err) }
-      if (!user) { return done(null, false) }
-      if (!user.verifyPassword(password)) { return done(null, false) }
+// passport.use(new LocalStrategy(
+//   (username, password, done) => {
+//     modelUser.findOne({ username: username }, (err, user) => {
+//       if (err) { return done(err) }
+//       if (!user) { return done(null, false) }
+//       if (!user.verifyPassword(password)) { return done(null, false) }
 
-      return done(null, user)
-    })
-  }
-))
-passport.serializeUser((user, done) => {
-  // console.log('serializeUser')
-  done(null, user)
-})
-passport.deserializeUser((user, done) => {
-  console.log('deserializeUser')
-  done(null, user)
-})
+//       return done(null, user)
+//     })
+//   }
+// ))
+// passport.serializeUser((user, done) => {
+//   done(null, user)
+// })
+// passport.deserializeUser((user, done) => {
+//   console.log('deserializeUser')
+//   done(null, user)
+// })
 
 app.get('/', (req, res) => {
   res.send('welcome to web API!')
 })
-app.post('/api/login',
-  passport.authenticate('local', { session: true }),
-  (req, res) => {
-    res.send(req.session.passport.user)
-  }
-)
-app.post('/api/logout', function (req, res) {
-  req.logout()
-  req.session.destroy()
-  // req.session.reload(function(err) {
-  //   // session updated
-  // })
-})
+// app.post('/api/login',
+//   passport.authenticate('local', { session: true }),
+//   (req, res) => {
+//     res.send(req.session.passport.user)
+//   }
+// )
+// app.post('/api/logout', function (req, res) {
+//   req.logout()
+//   req.session.destroy()
+// })
 
 //  ********   Routes define ***************
 const InputMaterialAndCostRouter = require('./Routes/InputMaterialAndCostRoutes')(modelInputMaterialAndCost)
