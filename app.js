711c049300c36312e67ebf782e8a91c656c2eba4
Kittisak Pharaphan <kungpotato@users.noreply.github.com>
2019-03-10 11:17:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ db = mongoose.connect(dbURI,{useNewUrlParser: true},function(err){
 })
 
 //  ********   Model define ***************
-// var modelInputMaterialAndCost = require('./models/InputMaterialAndCost');
+var modelInputMaterialAndCost = require('./models/InputMaterialAndCost');
 var modelDepartment = require('./models/MasterDepartment');
 var modelMaterial = require('./models/MasterMaterial');
 var modelUnit = require('./models/MasterUnit');
@@ -43,62 +43,62 @@ var port = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-// app.use(cookieParser());
-// app.use(session({
-//   name: 'JSESSION',
-//   secret: 'kungpotato',
-//   store: new MemoryStore(60 * 60 * 12),
-//   resave: true,
-//   saveUninitialized: true
-// }));
-// app.use(passport.initialize());
-// app.use(passport.session());
+app.use(cookieParser());
+app.use(session({
+  name: 'JSESSION',
+  secret: 'kungpotato',
+  store: new MemoryStore(60 * 60 * 12),
+  resave: true,
+  saveUninitialized: true
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 
-// passport.use(new LocalStrategy(
-//   function(username, password, done) {
-//     modelUser.findOne({ username: username }, function (err, user) {
-//       //console.log(user)
-//       if (err) { return done(err); }
-//       if (!user) { return done(null, false); }
-//       if (!user.verifyPassword(password)) { return done(null, false); }
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    modelUser.findOne({ username: username }, function (err, user) {
+      //console.log(user)
+      if (err) { return done(err); }
+      if (!user) { return done(null, false); }
+      if (!user.verifyPassword(password)) { return done(null, false); }
 
-//       return done(null, user);
-//     });
-//   }
-// ));
-// passport.serializeUser(function(user, done) {
-//   done(null, user);
-// });
-// passport.deserializeUser(function(user, done) {
-//   console.log('deserializeUser')
-//   done(null, user);
-// });
+      return done(null, user);
+    });
+  }
+));
+passport.serializeUser(function(user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  console.log('deserializeUser')
+  done(null, user);
+});
 
 app.get('/', function(req, res){
     res.send('welcome to web API!');
 });
-// app.post('/api/login',
-//   passport.authenticate('local', { session: true }),
-//   function(req, res) {
-//     res.send(req.session.passport.user);
-//   }
-// );
-// app.post('/api/logout', function (req, res) {
-//   req.logout()
-//   req.session.destroy(function (err) {
-//     res.redirect("/")
-//   })
-// })
+app.post('/api/login',
+  passport.authenticate('local', { session: true }),
+  function(req, res) {
+    res.send(req.session.passport.user);
+  }
+);
+app.post('/api/logout', function (req, res) {
+  req.logout()
+  req.session.destroy(function (err) {
+    res.redirect("/")
+  })
+})
 
 //  ********   Routes define ***************
-// InputMaterialAndCostRouter = require('./Routes/InputMaterialAndCostRoutes')(modelInputMaterialAndCost)
+InputMaterialAndCostRouter = require('./Routes/InputMaterialAndCostRoutes')(modelInputMaterialAndCost)
 DepartmentRouter = require('./Routes/deptRoutes')(modelDepartment)
 MaterialRouter = require('./Routes/materialRoutes')(modelMaterial)
 UnitRouter =  require('./Routes/unitRoutes')(modelUnit)
 UserRouter =  require('./Routes/userRoutes')(modelUser)
 
-// app.use('/api/input', InputMaterialAndCostRouter);
+app.use('/api/input', InputMaterialAndCostRouter);
 app.use('/api/department', DepartmentRouter);
 app.use('/api/material', MaterialRouter);
 app.use('/api/unit', UnitRouter);
